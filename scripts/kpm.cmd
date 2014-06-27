4eef2477a35fe4c3b5a2fc262eb33fd69539baf3
Wei Wang <chengtian.wang@gmail.com>
2014-06-26 18:17:48 -0700

    Make sure "/?" doesn't take over control in top level script

--- scripts/kpm.cmd
+++ scripts/kpm.cmd
@@ -6,7 +6,10 @@ REM <dev>
 @Echo ON
 REM </dev>
 
-CALL "%~dp0KLR.cmd" --lib "%~dp0;%~dp0lib\Microsoft.Framework.PackageManager" "Microsoft.Framework.PackageManager" %*
+SET ARGS=%*
+IF NOT "%ARGS%"=="" SET ARGS=%ARGS:/?="/?"%
+
+CALL "%~dp0KLR.cmd" --lib "%~dp0;%~dp0lib\Microsoft.Framework.PackageManager" "Microsoft.Framework.PackageManager" %ARGS%
 
 exit /b %ERRORLEVEL%
 ENDLOCAL
